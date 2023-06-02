8376228ef4c7747f5d3adce64a984627fa5d0420
Dhatri Acharya <Dhatri.Acharya@civica2.com>
2023-06-02 16:35:29 +0530

    add quit command

--- CoffeeShop.ShopInfoTool/CoffeeShop.ShopInfoTool/Program.cs
+++ CoffeeShop.ShopInfoTool/CoffeeShop.ShopInfoTool/Program.cs
@@ -5,7 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to WiredBrainCoffee Shopping info tool. ");
-            Console.ReadLine(); 
+            Console.ReadLine();
+            Console.WriteLine(" quit  exit application");
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (string.Equals("quit", command, StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+            }
         }
     }
 }
