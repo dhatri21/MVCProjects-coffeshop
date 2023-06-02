28c39282b7e4dd5bec6fdf37f48ae48a63264583
Dhatri Acharya <Dhatri.Acharya@civica2.com>
2023-06-02 12:27:32 +0530

    Add project files.

---
+++ CoffeeShop.ShopInfoTool/CoffeeShop.ShopInfoTool/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CoffeeShop.ShopInfoTool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to WiredBrainCoffee Shopping info tool. ");
+            Console.ReadLine(); 
+        }
+    }
+}
