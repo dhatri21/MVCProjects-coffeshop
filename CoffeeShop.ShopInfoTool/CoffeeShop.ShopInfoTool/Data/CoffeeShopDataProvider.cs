ca947d92ce53e81feb761291d475cdcf244d10dd
Dhatri Acharya <Dhatri.Acharya@civica2.com>
2023-06-02 12:41:08 +0530

    Add coffee shop data provider

---
+++ CoffeeShop.ShopInfoTool/CoffeeShop.ShopInfoTool/Data/CoffeeShopDataProvider.cs
@@ -0,0 +1,15 @@
+﻿using CoffeeShop.ShopInfoTool.Model;
+namespace CoffeeShop.ShopInfoTool.Data;
+
+public class CoffeeShopDataProvider
+{
+    public IEnumerable<CoffeeShop> LoadShops()
+    {
+        return new CoffeeShop[]
+        {
+            new CoffeeShop("Frankfurt",45),
+            new CoffeeShop("Zurich",39),
+            new CoffeeShop("Rome",25),
+        };
+    }
+}
